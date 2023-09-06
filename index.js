7b164ca787129cb81b5fd6d5aa7dd31718238a97
VedantDewangan <135214899+VedantDewangan@users.noreply.github.com>
2023-09-06 18:12:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var img1 = "./images/dice"+randomNumber1+".png";
+document.getElementById("img1").setAttribute("src",img1);
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var img2 = "./images/dice"+randomNumber2+".png";
+document.getElementById("img2").setAttribute("src",img2);
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 1 WON!!";
+}
+else if(randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 2 WON!!";
+}
+else
+{
+    document.querySelector("h1").innerHTML="DRAW!!";
+}
